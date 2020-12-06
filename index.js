84ed7fc663f5629427345e30064de25a4a8d0021
Ben Prat <bvprat@gmail.com>
2020-12-06 13:06:33 -0700

    Resolve path naming error.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const http = require('http');
+const Game = require('./src/Game');
 let app = http.createServer();
 
 // Start the server on port 3000
-app.listen(3000, '127.0.0.1');  
-console.log('Node server running on port 3000'); 
-
-const Game = require('../flashcards/src/Game.js');
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000')
 
 const currentGame = new Game();
 currentGame.start();
-
